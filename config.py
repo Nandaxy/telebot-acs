0cff33be6ea8605bab8b9bdc932a4ff6070a498d
Nandaxy <indri.khaulia12@gmail.com>
2025-01-12 08:29:57 +0700

    .

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-TELEGRAM_TOKEN = "7598281224:AAFjQlmQzjc_O11Zc4WyMloni_Gnv4imTaQ"
-#API_URL = "http://localhost:7557"
-API_URL = "http://hd008b5378p.sn.mynetname.net:5570"
+TELEGRAM_TOKEN = ""
+API_URL = "http://localhost:7557"
 SSIDKE = 1
